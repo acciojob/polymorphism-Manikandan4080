103e44f8c4d53d93d8d0e6b24e5307568efb7574
Manikandan <manikandan400800@gmail.com>
2023-06-09 10:09:35 +0530

    Polymorphism example

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public Product(){}
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product (int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Product p = new Product();
+        System.out.println(p.product(10, 2));
+        System.out.println(p.product(10, 8, 57));
+        System.out.println(p.product(50.1, 80.5));
+    }
 
 }
